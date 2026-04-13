28c6fe78068c37828f5e07dad047499d5f088398
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2026-04-13 19:34:21 -0300

    modificaciones a estilos de pokedex

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -12,7 +12,11 @@ const Pokedex = () => {
   if (loadingEntry || !entry) return <div>Cargando...</div>; 
 
   return (
-    <div className='flex w-full flex-col md:flex-row md:flex-wrap md:justify-center border border-black border-solid bg-emerald-100 xl:w-9/12'>
+    <div className='flex w-full flex-col md:flex-row md:flex-wrap md:justify-center xl:w-9/12 xl:my-2
+    border-black border-solid border-4 rounded-lg bg-linear-to-b from-yellow-300 via-black to-red-500
+    elegance:from-elegance-400 elegance:to-elegance-600 elegance:text-white elegance:border-white
+    dark:from-gray-400 dark:to-gray-700 dark:text-white
+    radiation:from-radiation-500 radiation:to-radiation-700 radiation:text-radiation-50'>
 
       <NameForm/>
       <PokedexEntryCard/>
