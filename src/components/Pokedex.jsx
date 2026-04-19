32bd84622e88c2f34e81118ebe29fd4c72632c01
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2026-04-19 20:41:22 -0300

    ligero cambio en el codigo de pokedex

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -8,11 +8,9 @@ const Pokedex = () => {
 
     const { entry, loadingEntry } = usePokedexContext();
 
-    // Early return mientras carga o si no hay datos
-  if (loadingEntry || !entry) return <div>Cargando...</div>; 
 
   return (
-    <div className='flex w-full flex-col md:flex-row md:flex-wrap md:justify-center xl:w-9/12 xl:my-2
+    <> {entry ? <div className='flex w-full flex-col md:flex-row md:flex-wrap md:justify-center xl:w-9/12 xl:my-2
     border-black border-solid border-4 rounded-lg bg-linear-to-b from-yellow-300 via-black to-red-500
     elegance:from-elegance-400 elegance:to-elegance-600 elegance:text-white elegance:border-white
     dark:from-gray-400 dark:to-gray-700 dark:text-white
@@ -22,7 +20,9 @@ const Pokedex = () => {
       <PokedexEntryCard/>
       <DexEntriesList/>
     
-    </div>
+    </div> : ""}
+    </>
+    
   )
 }
 
